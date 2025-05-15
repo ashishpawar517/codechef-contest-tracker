417b7ba953f0ad4a241b1b4c7a04b3cde76e66c1
Aashish Pawar <ashishpawar517.icloud@gmail.com>
2025-05-15 20:01:38 +1000

    feat: update layout.tsx with icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,18 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "CodeChef Contest Tracker",
-  description: "Track CodeChef contests and problems",
-    generator: 'v0.dev'
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
+      <head>
+        <link rel="icon"
+        href="https://lh3.googleusercontent.com/a/ACg8ocKrIUGopTDJ7wJfCmyNSPAnXyQAqxDBrwVAJ4eGfEqo7BlMagY=s576-c-no"
+        type="image/png"/>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
