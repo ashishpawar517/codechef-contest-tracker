e400c702be2f60e3a8f9d0aa48e2f6e9e70a663a
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2025-05-15 19:20:22 +1000

    feat: create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import ContestTracker from "@/components/contest-tracker"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white text-gray-800">
+      <ContestTracker />
+    </main>
+  )
+}
